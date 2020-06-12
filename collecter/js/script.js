d2e30596d44df968f371b9dd67a71cafd4299aab
alex.chu <alex.chu@emali.io>
2020-06-12 15:13:34 +0800

    issueUpdate

--- collecter/js/script.js
+++ collecter/js/script.js
@@ -70,7 +70,7 @@ function sendToTester() {
                         animation: 'top',
                         closeAnimation: 'top',
                         backgroundDismiss: true,
-                        content:  `Record sent, will redirect to sample list page`,
+                        content:  `Record sent, redirect to sample list page`,
                         onOpen: function () {
                             var self = this;
                             setTimeout(function () {
@@ -117,5 +117,147 @@ function saveTest(batchNo) {
             }, 600);
         },
         
+    });
+}
+
+function issueCred() {
+    $.confirm({
+        title: 'Issue Credential',
+        columnClass: 'col-md-6 col-sm-12',
+        content: `
+        <div class="row">
+            <div class="col-6 mb-4">
+                <span class="info-title">Full Name</span>
+                <span class="info-value">Bernadette</span>
+            </div>
+            <div class="col-6 mb-4">
+                <span class="info-title">HKID</span>
+                <span class="info-value">A1234567(2)</span>
+            </div>
+            <div class="col-4">
+            <span class="info-title">PCR</span>
+            <span class="info-value">+ Positive</span>
+        </div>
+        <div class="col-4">
+            <span class="info-title">IgG</span>
+            <span class="info-value">+ Positive</span>
+        </div>
+        <div class="col-4">
+            <span class="info-title">IgM</span>
+            <span class="info-value">- Negative</span>
+        </div>
+        </div>
+        
+
+       
+        `,
+        type: 'green',
+        animation: 'top',
+        closeAnimation: 'bottom',
+        onOpenBefore: function () {
+            $(".jconfirm-buttons").addClass("form-row col-12 pl-0 pr-0");
+        },
+        // autoClose: 'Cancel|8000',
+        buttons: {
+            Cancel: {
+                text: 'Cancel',
+                btnClass: 'btn btn-default col',
+                action: close()
+            },
+            Send: {
+                text: 'Issue',
+                btnClass: 'btn btn-green col',
+                action: function () {
+                    $.dialog({
+                        title: 'Credential Issued',
+                        columnClass: 'col-6',
+                        type: 'green',
+                        animation: 'top',
+                        closeAnimation: 'top',
+                        backgroundDismiss: true,
+                        content:  `Credential Issued, redirect to sample list page`,
+                        onOpen: function () {
+                            var self = this;
+                            setTimeout(function () {
+                                self.close();
+                                location.href="sampleList.html"
+                            }, 1500);
+                        },
+                        
+                    });
+                    
+                }
+            }
+        }
+    });
+}
+
+function issueCredBatch() {
+    $.confirm({
+        title: 'Issue Credential',
+        columnClass: 'col-md-6 col-sm-12',
+        content: `
+        <div class="row">
+            <div class="col-6 mb-4">
+                <span class="info-title">Full Name</span>
+                <span class="info-value">Bernadette</span>
+            </div>
+            <div class="col-6 mb-4">
+                <span class="info-title">HKID</span>
+                <span class="info-value">A1234567(2)</span>
+            </div>
+
+            <div class="col-6 mb-4">
+                <span class="info-title">Full Name</span>
+                <span class="info-value">Howard</span>
+            </div>
+            <div class="col-6 mb-4">
+                <span class="info-title">HKID</span>
+                <span class="info-value">A1234567(6)</span>
+            </div>
+            
+        </div>
+        
+
+       
+        `,
+        type: 'green',
+        animation: 'top',
+        closeAnimation: 'bottom',
+        onOpenBefore: function () {
+            $(".jconfirm-buttons").addClass("form-row col-12 pl-0 pr-0");
+        },
+        // autoClose: 'Cancel|8000',
+        buttons: {
+            Cancel: {
+                text: 'Cancel',
+                btnClass: 'btn btn-default col',
+                action: close()
+            },
+            Send: {
+                text: 'Issue',
+                btnClass: 'btn btn-green col',
+                action: function () {
+                    $.dialog({
+                        title: 'Credential Issued',
+                        columnClass: 'col-6',
+                        type: 'green',
+                        animation: 'top',
+                        closeAnimation: 'top',
+                        backgroundDismiss: true,
+                        content:  `Credential Issued, redirect to sample list page`,
+                        onOpen: function () {
+                            var self = this;
+                            setTimeout(function () {
+                                self.close();
+                                location.href="sampleList.html"
+                            }, 1500);
+                        },
+                        
+                    });
+                    
+                }
+            }
+        }
     });
 }
