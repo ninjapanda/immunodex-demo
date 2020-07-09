812d538b1764acd3d4634c05bfbfba3e28a8018a
chulaptung <alex.chu@emali.io>
2020-07-09 18:34:15 +0800

    statusUpdate

--- issuer/js/checkInList.js
+++ issuer/js/checkInList.js
@@ -24,16 +24,91 @@ $(document).ready(function () {
             [1, 'asc']
         ]
     });
-    setAsDatatable();
+    var tableList = ['checkInList_all']
+    var tableList2 = ['checkInList_pending']
+    setAsDatatable(tableList);
+    setAsDatatable2(tableList2);
 });
 
-function setAsDatatable() {
+function setAsDatatable(tableList) {
     var removeBtn = `<div class="btn remove"><img src="img/icon/bin.svg">Remove selected</button>`;
-    for (i = 1; i <= 1; i++) {
-        $("#checkInList_" + i).DataTable({
-            initComplete: function () {
-                $("#checkInList_" + i + "_filter").append(removeBtn);
-            },
+
+    for (i = 0; i < tableList.length; i++) {
+        $("#"+tableList[i]).DataTable({
+            initComplete: function(){
+                $("#"+tableList[i]+"_filter").append();},
         });
     }
+}
+
+function setAsDatatable2(tableList) {
+    var removeBtn = `<div class="btn remove"><img src="img/icon/bin.svg">Remove selected</button>`;
+    var IssueBtn = `<div onclick="sendBatchCred()" class="btn issue mr-2">Send to selected</button>`;
+
+    for (i = 0; i < tableList.length; i++) {
+        $("#"+tableList[i]).DataTable({
+            initComplete: function(){
+                $("#"+tableList[i]+"_filter").append(IssueBtn);},
+        });
+    }
+}
+
+function sendBatchCred(){
+    $.confirm({
+        title: 'Notify User',
+        columnClass: 'col-6',
+        content: `
+        <div class="row" id="notifyUser">
+            <div class="col-4">
+                <span class="font-weight-bold">Assessment ID</span>
+            </div>
+            <div class="col-4">
+                <span class="font-weight-bold">Full Name</span>
+            </div>
+            <div class="col-4">
+                <span class="font-weight-bold">Mobile No.</span>
+            </div>
+
+            <div class="col-4">
+                <span>00122</span>
+            </div>
+            <div class="col-4">
+                <span>Sherlock</span>
+            </div>
+            <div class="col-4">
+                <span>0 208 224 3692</span>
+            </div>
+
+            <div class="col-4">
+                <span>00132</span>
+            </div>
+            <div class="col-4">
+                <span>Holmes</span>
+            </div>
+            <div class="col-4">
+                <span>0 123 423 9123</span>
+            </div>
+            
+        </div>
+        `,
+        type: 'blue',
+        animation: 'top',
+        closeAnimation: 'bottom',
+        onOpenBefore: function () {
+            $(".jconfirm-buttons").addClass("form-row col-12 pl-0 pr-0");
+        },
+        // autoClose: 'Cancel|8000',
+        buttons: {
+            Cancel: {
+                text: 'Cancel',
+                btnClass: 'btn btn-default col',
+                action: close()
+            },
+            deny: {
+                text: 'Confirm',
+                btnClass: 'btn col',
+                action: function () {}
+            }
+        }
+    });
 }
--- issuer/js/testRecord.js
+++ issuer/js/testRecord.js
@@ -1,5 +1,130 @@
 $(document).ready(function () {
 
     $("#testRecord_1").DataTable();
+
+    $.extend(true, $.fn.dataTable.defaults, {
+        "lengthChange": false,
+        dom: 'l<"toolbar">frtip',
+        language: {
+            search: "_INPUT_",
+            searchPlaceholder: "Search"
+        },
+        'columnDefs': [{
+            'targets': 0,
+            "ordering": false,
+            'checkboxes': {
+                'selectRow': true
+            }
+        }, {
+            orderable: false,
+            targets: -1
+        }],
+        'select': {
+            'style': 'multi'
+        },
+        'order': [
+            [1, 'asc']
+        ]
+    });
+    var tableList = ['testPendingIssue']
+    setAsDatatable(tableList);
 });
 
+function IssueCred() {
+    $.confirm({
+        title: 'Issue Credentail',
+        columnClass: 'col-6',
+        content: `
+        <span class="info-title">Assessment ID</span>
+        <span class="info-value">00131</span>
+        
+        <span class="info-title">Full Name</span>
+        <span class="info-value">Sherlock Holmes</span>
+        <span class="info-title">Mobile No.</span>
+        <span class="info-value">0 207 224 3688</span>
+        `,
+        type: 'blue',
+        animation: 'top',
+        closeAnimation: 'bottom',
+        onOpenBefore: function () {
+            $(".jconfirm-buttons").addClass("form-row col-12 pl-0 pr-0");
+        },
+        autoClose: 'Cancel|8000',
+        buttons: {
+            Cancel: {
+                text: 'Cancel',
+                btnClass: 'btn btn-default col',
+                action: close()
+            },
+            deny: {
+                text: 'Confirm',
+                btnClass: 'btn col',
+                action: function () {
+                    location.href="testRecord.html"
+                }
+            }
+        }
+    });
+
+}
+
+function setAsDatatable(tableList) {
+    var IssueBtn = `<div onclick="IssueBatchCred()" class="btn issue mr-2">Issue to selected</button>`;
+
+    for (i = 0; i < tableList.length; i++) {
+        $("#"+tableList[i]).DataTable({
+            initComplete: function(){
+                $("#"+tableList[i]+"_filter").append(IssueBtn);},
+        });
+    }
+}
+
+function IssueBatchCred(){
+    $.confirm({
+        title: 'Issue Credential',
+        columnClass: 'col-6',
+        content: `
+        <div class="row" id="issueCred">
+            <div class="col-4">
+                <span class="font-weight-bold">Assessment ID</span>
+            </div>
+            <div class="col-4">
+                <span class="font-weight-bold">Full Name</span>
+            </div>
+            <div class="col-4">
+                <span class="font-weight-bold">Mobile No.</span>
+            </div>
+
+            <div class="col-4">
+                <span>00122</span>
+            </div>
+            <div class="col-4">
+                <span>Sherlock</span>
+            </div>
+            <div class="col-4">
+                <span>0 208 224 3692</span>
+            </div>
+            
+        </div>
+        `,
+        type: 'blue',
+        animation: 'top',
+        closeAnimation: 'bottom',
+        onOpenBefore: function () {
+            $(".jconfirm-buttons").addClass("form-row col-12 pl-0 pr-0");
+        },
+        // autoClose: 'Cancel|8000',
+        buttons: {
+            Cancel: {
+                text: 'Cancel',
+                btnClass: 'btn btn-default col',
+                action: close()
+            },
+            deny: {
+                text: 'Confirm',
+                btnClass: 'btn col',
+                action: function () {}
+            }
+        }
+    });
+}
